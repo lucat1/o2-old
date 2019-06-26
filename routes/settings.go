361841f90fe047648de8304984e96f7d273eb73d
LucaT1 <lucat1@users.noreply.github.com>
2019-06-26 18:05:33 +0200

    Implement repository renaming

--- routes/settings.go
+++ routes/settings.go
@@ -1,8 +1,11 @@
 package routes
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"github.com/lucat1/o2/shared"
+	"go.uber.org/zap"
 )
 
 // Settings renders the settings page for a repository
@@ -28,5 +31,46 @@ func Settings(c *gin.Context) {
 		})
 		return
 	} else {
+		owner := c.Param("user")
+		reponame := c.Param("repo")
+		// Method POST
+		if newName := c.PostForm("general-rename"); newName != "" && newName != reponame {
+			shared.GetLogger().Info(
+				"Renaming repository",
+				zap.String("owner", owner),
+				zap.String("from", reponame),
+				zap.String("to", newName),
+			)
+
+			dbRepo := findRepoInDatabase(owner, reponame)
+			if err := shared.GetDatabase().Model(dbRepo).Update("name", newName).Error; err != nil {
+				shared.GetLogger().Warn(
+					"Could not rename repository",
+					zap.String("owner", owner),
+					zap.String("from", reponame),
+					zap.String("to", newName),
+					zap.Error(err),
+				)
+				c.Status(500)
+				c.Abort()
+				return
+			}
+			if err := os.Rename(getRepositoryPath(owner, reponame), getRepositoryPath(owner, newName)); err != nil {
+				shared.GetLogger().Warn(
+					"Could not rename in the filesystem",
+					zap.String("owner", owner),
+					zap.String("from", reponame),
+					zap.String("to", newName),
+					zap.Error(err),
+				)
+				c.Status(500)
+				c.Abort()
+				return
+			}
+
+			c.Redirect(302, "/"+owner+"/"+newName)
+			return
+		}
+
 	}
 }
